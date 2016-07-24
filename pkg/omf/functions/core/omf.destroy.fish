dee2ae970676b2834f581919f3edcc1398f1cdf4
Stephen M. Coakley <me@stephencoakley.com>
2016-07-24 15:33:04 -0500

    Improve uninstall experience

--- pkg/omf/functions/core/omf.destroy.fish
+++ pkg/omf/functions/core/omf.destroy.fish
@@ -1,13 +1,4 @@
 function omf.destroy -d "Remove Oh My Fish"
-  echo (omf::dim)"Removing Oh My Fish..."(omf::off)
-
-  # Trigger uninstall events
-  set -l installed_package_path $OMF_PATH/pkg/*
-  for pkg in (basename $installed_package_path)
-    omf.packages.run_hook $path uninstall
-    emit uninstall_$pkg
-  end
-
   # Run the uninstaller
   fish "$OMF_PATH/bin/install" --uninstall "--path=$OMF_PATH" "--config=$OMF_CONFIG"
 
